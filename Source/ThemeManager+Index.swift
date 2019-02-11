98bc795ef87b285ca32e26fb30755f9e96ebf158
beemogmbh <github@beemo.eu>
2019-02-11 12:26:16 +0100

    Disable some logs

--- Source/ThemeManager+Index.swift
+++ Source/ThemeManager+Index.swift
@@ -13,7 +13,7 @@ extension ThemeManager {
     public class func colorElement(for array: [String]) -> UIColor? {
         guard let rgba = element(for: array) else { return nil }
         guard let color = try? UIColor(rgba_throws: rgba as String) else {
-            print("SwiftTheme WARNING: Not convert rgba \(rgba) in array: \(array)[\(currentThemeIndex)]")
+            //print("SwiftTheme WARNING: Not convert rgba \(rgba) in array: \(array)[\(currentThemeIndex)]")
             return nil
         }
         return color
@@ -22,7 +22,7 @@ extension ThemeManager {
     public class func imageElement(for array: [String]) -> UIImage? {
         guard let imageName = element(for: array) else { return nil }
         guard let image = UIImage(named: imageName as String) else {
-            print("SwiftTheme WARNING: Not found image name '\(imageName)' in array: \(array)[\(currentThemeIndex)]")
+            //print("SwiftTheme WARNING: Not found image name '\(imageName)' in array: \(array)[\(currentThemeIndex)]")
             return nil
         }
         return image
@@ -31,7 +31,7 @@ extension ThemeManager {
     public class func element<T>(for array: [T]) -> T? {
         let index = ThemeManager.currentThemeIndex
         guard  array.indices ~= index else {
-            print("SwiftTheme WARNING: Not found element in array: \(array)[\(currentThemeIndex)]")
+            //print("SwiftTheme WARNING: Not found element in array: \(array)[\(currentThemeIndex)]")
             return nil
         }
         return array[index]
